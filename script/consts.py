ca82598655798a27a61b27a7bd37215a9327392b
sonnyhcl <sonnyhcl@163.com>
2018-07-17 19:51:41 +0800

    finish get_city_polyline

---
+++ script/consts.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+
+CELL_RADIUS = 3
+EARTH_RADIUS = 6371
--- script/main.py
+++ script/main.py
@@ -1,7 +1,25 @@
 # -*- coding:utf-8 -*-
-from config import *
 from thrall.amap import session
+from thrall.amap.consts import ExtensionFlag
+from utils import *
 
-r = session.riding('116.434307,39.90909', destination=(116.434446,39.90816))
-print(r.data)
 
+def get_city_polyline(keywords):
+    """
+
+    :param keywords: 地名关键词
+    规则：只支持单个关键词语搜索关键词支持：行政区名称、citycode、adcode
+    :return:
+    """
+    result = session.district(keywords, sub_district=0, extensions=ExtensionFlag(1))
+    raw_polylines = result.data[0].polyline.split("|") if result.data else []
+
+    return [
+        Point(polyline)
+        for raw_polyline in raw_polylines
+        for polyline in raw_polyline.split(";")
+    ]
+
+
+if __name__ == "__main__":
+    print("上海边界: ", get_city_polyline("上海"))
